da31d77bedcd1387b231e73cd24faf7ac6a68b06
raimanu <68045625+raimanu@users.noreply.github.com>
2020-07-13 11:03:59 +1100

    Maintenant peut continuer toujours dans un sens et rerster dans le tableau

--- main.ts
+++ main.ts
@@ -4,7 +4,10 @@ input.onButtonPressed(Button.A, function () {
         led.plot(x - 1, y)
         x = x - 1
     } else if (x == 0 && y == 0) {
-    	
+        led.unplot(x, y)
+        led.plot(x + 4, y + 4)
+        y = 4
+        x = 4
     } else {
         led.unplot(x, y)
         led.plot(x + 4, y - 1)
@@ -18,7 +21,10 @@ input.onButtonPressed(Button.B, function () {
         led.plot(x + 1, y)
         x = x + 1
     } else if (x == 4 && y == 4) {
-    	
+        led.unplot(x, y)
+        led.plot(x * 0, y * 0)
+        y = 0
+        x = 0
     } else {
         led.unplot(x, y)
         led.plot(x * 0, y + 1)
@@ -29,8 +35,8 @@ input.onButtonPressed(Button.B, function () {
 let y = 0
 let x = 0
 x = 2
-y = 0
-led.plot(2, 0)
+y = 2
+led.plot(2, 2)
 basic.forever(function () {
 	
 })
